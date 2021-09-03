0b0f633aadb561d6fa8f9bfc13e3be134311add8
bgme <i@bgme.me>
2021-09-03 19:01:47 +0800

    update jjwxcFontDecode.ts

--- src/rules/lib/jjwxcFontDecode.ts
+++ src/rules/lib/jjwxcFontDecode.ts
@@ -32,12 +32,18 @@ async function getJjwxcFontTable(fontName: string) {
 async function fetchRemoteFont(fontName: string) {
   const url = `https://jjwxc.bgme.bid/${fontName}.json`;
   // const url = `https://jjwxc.lo.bgme.me/${fontName}.json`;
-  log.info(`[jjwxc-font]开始请求远程字体对照表 ${fontName}`);
-  const resp = await fetch(url);
-  if (resp.status === 200) {
-    log.info(`[jjwxc-font]远程字体对照表 ${fontName} 下载成功`);
-    return (await resp.json()) as jjwxcFontTable;
-  } else {
+  try {
+    log.info(`[jjwxc-font]开始请求远程字体对照表 ${fontName}`);
+    const resp = await fetch(url);
+    if (resp.status === 200) {
+      log.info(`[jjwxc-font]远程字体对照表 ${fontName} 下载成功`);
+      return (await resp.json()) as jjwxcFontTable;
+    } else {
+      log.info(`[jjwxc-font]远程字体对照表 ${fontName} 下载失败`);
+      return undefined;
+    }
+  } catch (error) {
+    log.error(error);
     log.info(`[jjwxc-font]远程字体对照表 ${fontName} 下载失败`);
     return undefined;
   }
