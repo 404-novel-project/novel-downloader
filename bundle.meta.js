863d803b00b359c15b710de61ead6f91b930ca89
yingziwu <i@bgme.me>
2021-10-09 14:48:04 +0000

    Deploying to gh-pages from @ yingziwu/novel-downloader@deff585192bdb9b2e5ff38f90f3adee722e36e65 🚀

--- bundle.meta.js
+++ bundle.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           小说下载器
-// @version        4.0.2.1633790728201
+// @version        4.0.2.1633790878044
 // @author         bgme
 // @description    一个可扩展的通用型小说下载器。
 // @supportURL     https://github.com/yingziwu/novel-downloader
--- tester.meta.js
+++ tester.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        小说下载器测试脚本
-// @version     4.0.2.1633790733777
+// @version     4.0.2.1633790883375
 // @author      bgme
 // @description 小说下载器测试脚本
 // @match       *://www.ciweimao.com/chapter-list/*
