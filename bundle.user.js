2837c4dea51d799edd6c35e3461ec157efe43d32
yingziwu <i@bgme.me>
2021-12-13 10:35:23 +0000

    Deploying to gh-pages from @ yingziwu/novel-downloader@a6303680999d9267ec15c79f710a3bb873a4bf9c 🚀

--- bundle.user.js
+++ bundle.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           小说下载器
-// @version        4.5.5.408
+// @version        4.5.5.410
 // @author         bgme
 // @description    一个可扩展的通用型小说下载器。
 // @supportURL     https://github.com/yingziwu/novel-downloader
