0b236c9cc2fbaba0e3cb90290adf9e026ff8fd7f
bgme <i@bgme.me>
2021-12-23 18:28:34 +0800

    fix github CI

---
+++ src/htmlcss.d.ts
@@ -0,0 +1,19 @@
+declare module "*.html" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.html.j2" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.css" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.less" {
+  const content: string;
+  export default content;
+}
