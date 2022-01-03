6a7f9eb475662a237356b5dfc930cd9c4215e9b4
yingziwu <i@bgme.me>
2022-01-03 11:28:41 +0000

    Deploying to gh-pages from @ yingziwu/novel-downloader@29c74d1c96d02e5fa7b5b0b1b7d07ef109f487fd 🚀

--- tester.meta.js
+++ tester.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        小说下载器测试脚本
-// @version     4.7.7.474
+// @version     4.7.7.475
 // @author      bgme
 // @description 小说下载器测试脚本
 // @match       *://www.ciweimao.com/chapter-list/*
