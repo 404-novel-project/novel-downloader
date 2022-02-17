cf794cc38f624e79a6a74cbdf4ca6c90df21e9ae
yingziwu <i@bgme.me>
2022-02-17 11:07:43 +0000

    Deploying to gh-pages from @ 404-novel-project/novel-downloader@b4f7b519c6d76f78ecff0818d2164266c8ba3510 🚀

--- bundle-greasyfork.meta.js
+++ bundle-greasyfork.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           小说下载器
-// @version        4.8.3.590
+// @version        4.8.3.591
 // @author         bgme
 // @description    一个可扩展的通用型小说下载器。
 // @supportURL     https://github.com/404-novel-project/novel-downloader
