4a14456c1439afeacebcecb7bbab1895cd880a7e
yingziwu <i@bgme.me>
2022-03-04 01:45:26 +0000

    Deploying to gh-pages from @ 404-novel-project/novel-downloader@b38fcec04b6c89a45caf12a67c99913f09145858 🚀

--- bundle.user.js
+++ bundle.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           小说下载器
-// @version        4.8.3.621
+// @version        4.8.3.622
 // @author         bgme
 // @description    一个可扩展的通用型小说下载器。
 // @supportURL     https://github.com/404-novel-project/novel-downloader
