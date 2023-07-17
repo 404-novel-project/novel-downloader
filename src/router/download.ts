0f8e1bf0c00c20215b7ef8444bd421ae537c986b
Seraphoenixy <1264310400@qq.com>
2023-07-18 00:40:57 +0800

    独阅读的域名变更，对其进行了修改

--- src/router/download.ts
+++ src/router/download.ts
@@ -206,7 +206,7 @@ export async function getRule(): Promise<BaseRuleClass> {
       ruleClass = Iqingguo;
       break;
     }
-    case "www.duread8.com": {
+    case "www.duread.cn": {
       const { Duread } = await import("../rules/special/original/ciweimao");
       ruleClass = Duread;
       break;
