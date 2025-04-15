a9908bad4fb0089be76d96d51e199314fe12f756
ldm0206 <ldm200026@live.cn>
2025-04-15 10:16:23 +0000

    Deploying to gh-pages from @ 404-novel-project/novel-downloader@3826b82b1619c18b59a59f35c0f0ed55d9a72ac5 🚀

--- bundle-greasyfork.user.js
+++ bundle-greasyfork.user.js
@@ -5,7 +5,7 @@
 // @description    一个可扩展的通用型小说下载器。
 // @description:en An scalable universal novel downloader.
 // @description:ja スケーラブルなユニバーサル小説ダウンローダー。
-// @version        5.2.1107
+// @version        5.2.1108
 // @author         bgme
 // @supportURL     https://github.com/404-novel-project/novel-downloader
 // @exclude        *://www.jjwxc.net/onebook.php?novelid=*&chapterid=*
@@ -32657,7 +32657,7 @@ class Pixiv extends _rules__WEBPACK_IMPORTED_MODULE_0__/* .BaseRuleClass */ .Q {
                     bookUrl,
                     bookname,
                     chapterUrl: bookUrl,
-                    chapterNumber: 0,
+                    chapterNumber: 1,
                     chapterName: bookname,
                     isVIP: false,
                     isPaid: false,
