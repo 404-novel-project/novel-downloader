cb21d7140ffb67aeda4c052c76795f6c6e8e4317
Otaro <9078877+oovz@users.noreply.github.com>
2025-05-13 23:11:53 -0500

    style: zhenhunxiaoshuo.ts

--- src/rules/onePage/zhenhunxiaoshuo.ts
+++ src/rules/onePage/zhenhunxiaoshuo.ts
@@ -4,14 +4,21 @@ export const zhenhunxiaoshuo = () =>
   mkRuleClass({
     bookUrl: document.location.href,
     bookname: (document.querySelector("h1") as HTMLElement)?.innerText.trim(),
-    author: (document.querySelector("div.focusbox-text > p") as HTMLParagraphElement)?.innerText.split('\n')[0]?.split('作者：')[1] || "",
+    author:
+      (
+        document.querySelector("div.focusbox-text > p") as HTMLParagraphElement
+      )?.innerText
+        .split("\n")[0]
+        ?.split("作者：")[1] || "",
     introDom: (() => {
-      const introElement = document.createElement('p');
+      const introElement = document.createElement("p");
 
-      const introParagraph = document.querySelector("div.focusbox-text > p") as HTMLParagraphElement;
+      const introParagraph = document.querySelector(
+        "div.focusbox-text > p",
+      ) as HTMLParagraphElement;
       if (introParagraph) {
         // always after <br> tag
-        const introText = introParagraph.innerText.split('\n')[1] || "";
+        const introText = introParagraph.innerText.split("\n")[1] || "";
         introElement.textContent = introText;
       }
 
@@ -20,10 +27,11 @@ export const zhenhunxiaoshuo = () =>
     introDomPatch: (introDom) => introDom,
     coverUrl: null,
     aList: document.querySelectorAll("article > a"),
-    getContent: (doc) => doc.querySelector("article.article-content") as HTMLElement,
+    getContent: (doc) =>
+      doc.querySelector("article.article-content") as HTMLElement,
     contentPatch: (content) => content,
-    // concurrencyLimit: 3, 
-    // sleepTime: 1000,  
-    language: "zh",   
-    // nsfw: true,       
+    // concurrencyLimit: 3,
+    // sleepTime: 1000,
+    language: "zh",
+    // nsfw: true,
   });
