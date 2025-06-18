373be92a7927c3de11dc7a0eb6c4d5b4ed877a95
Otaro <9078877+oovz@users.noreply.github.com>
2025-06-17 21:22:42 -0500

    fix: 腐小书 (#811)
    * fix: 腐小书
    fxshu.top

--- src/rules/onePage/fuxiaoshu.ts
+++ src/rules/onePage/fuxiaoshu.ts
@@ -1,19 +1,14 @@
 import { mkRuleClass } from "./template";
 
 export const fuxiaoshu = mkRuleClass({
-    bookUrl: document.location.href,
-    bookname: (
-        document.querySelector(".con_box h1") as HTMLHeadElement
-    ).innerText.trim(),
-    author: (
-        document.querySelector(".con_box .tits strong a") as
-        | HTMLAnchorElement
-        | HTMLSpanElement
-    ).innerText
-        .trim(),
-    introDom: undefined,
-    coverUrl: null,
-    aList: Array.from(document.querySelectorAll(".alt_page li > a")).slice(2),
-    getContent: (doc) => doc.querySelector("div.co-bay") as HTMLElement,
-    contentPatch: (content) => content,
+  bookUrl: document.location.href,
+  bookname: (document.querySelector("h1") as HTMLHeadElement).innerText.trim(),
+  author: (
+    document.querySelector("div.af_lst > strong > a") as HTMLAnchorElement
+  )?.innerText.trim(),
+  introDom: undefined,
+  coverUrl: null,
+  aList: Array.from(document.querySelectorAll(".alt_page li > a")).slice(2), // 前2个是 “共x页”和“下一页”，从第3个开始
+  getContent: (doc) => doc.querySelector("div.wznrb") as HTMLElement,
+  contentPatch: (content) => content,
 });
