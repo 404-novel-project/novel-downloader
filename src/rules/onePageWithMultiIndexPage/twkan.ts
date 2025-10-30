e4de146e4711712db305996ded9a778f1941d6e1
debin <touch767@gmail.com>
2025-10-30 18:06:05 +0800

    twkan更换目录页

--- src/rules/onePageWithMultiIndexPage/twkan.ts
+++ src/rules/onePageWithMultiIndexPage/twkan.ts
@@ -20,17 +20,30 @@ export const twkan = () =>
       const menuUrl = (
         document.querySelector('a.btn.more-btn[href]') as HTMLAnchorElement
       ).href;
-      const doc = await getHtmlDOM(menuUrl, "UTF-8");
+      //https://twkan.com/book/xxx/index.html
+      const article = menuUrl.match(/\/book\/(\d+)\/index.html/);
+      const articleId = article?article[1]:""
+      const menuUrlNew = "https://twkan.com/ajax_novels/chapterlist/" + articleId + ".html"
+
+      const doc = await getHtmlDOM(menuUrlNew, "UTF-8");
       indexPages.push(doc);
       return indexPages;
     },
-    getAList: (doc) => Array.from(doc.querySelectorAll("#allchapter ul a")) as unknown as NodeListOf<Element>,
+    getAList: (doc) => Array.from(doc.querySelectorAll("ul a")) as unknown as NodeListOf<Element>,
     getAName: (aElem) => (aElem as HTMLElement).innerText.trim(),
-    getContent: (doc) => doc.querySelector(".txtnav"),
+    getContent: (doc) => doc.querySelector("#txtcontent0"),
     contentPatch: (content) => {
       rm(".hide720, .txtcenter, .bottom-ad", true, content);
       // rm2([/^谷[\u4e00-\u9fa5]{0,1}$/gm], content);
-      rm2([/【.*台灣小說網.*】/g,/（.*台灣小說網.*）/g,/本書由.*首發/g,/本書首發.*讀體驗/g,/GOOGLE搜索TWKAN/gi,/.*台灣小說網.*(隨時享|超實用)/g], content);
+      rm2([
+        /【.*台灣小說網.*】/g,
+        /（.*台灣小說網.*）/g,
+        /本書由.*首發/g,
+        /本書首發.*讀體驗/g,
+        /GOOGLE搜索TWKAN/gi,
+        /.*台灣小說網.*(隨時.|超全|超讚|超實用|超順暢|超流暢|超方便|超好用|超貼心|超給力|超省心|超靠譜|輕鬆讀|輕鬆看|任你.|等你尋)/g,
+        /\(本章完\)/g
+        ], content);
     
       // Replace text nodes between <p> nodes with <p> nodes
       const walker = document.createTreeWalker(content, NodeFilter.SHOW_TEXT, null);
