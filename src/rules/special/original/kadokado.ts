6b8692bd925efd9b8b3bd764decbbe33e877fb60
ldm2060 <ldm200026@live.cn>
2026-05-04 18:49:53 +0800

    fix #929

--- src/rules/special/original/kadokado.ts
+++ src/rules/special/original/kadokado.ts
@@ -1,107 +1,166 @@
 import { BaseRuleClass, ChapterParseObject } from "../../../rules";
-import * as CryptoJS from "crypto-js";
 import { Book, BookAdditionalMetadate } from "../../../main/Book";
 import { cleanDOM } from "../../../lib/cleanDOM";
 import { Chapter, ChapterAdditionalMetadate } from "../../../main/Chapter";
 import { getAttachment } from "../../../lib/attachments";
 import { log } from "../../../log";
-import { ggetHtmlDOM, getFrameContentEvent } from "../../../lib/http";
-import { rm } from "../../../lib/dom";
+import { ggetText } from "../../../lib/http";
+
+interface KadokadoCollection {
+  collectionId: number;
+  collectionDisplayName: string;
+  chapters: KadokadoChapter[];
+}
+
+interface KadokadoChapter {
+  chapterId: number;
+  chapterDisplayName: string;
+  isFree: boolean;
+  points: number;
+}
+
+interface KadokadoTitleInfo {
+  titleId: number;
+  ownerId: number;
+  displayName: string;
+  logline: string;
+  tags: string[];
+  coverUrls: string[];
+  ownerDisplayName: string;
+  authors: { id: number; displayName: string }[];
+  totalChapterCount: number;
+  firstChapterId: number;
+  collectionEpisodes: { collectionId: number; displayName: string }[];
+}
+
+interface KadokadoChapterContent {
+  type: string;
+  content: string;
+  imageUrls: string[];
+}
+
 export class kadokado extends BaseRuleClass {
-    public constructor() {
-        super();
-        this.maxRunLimit = 1;
-        this.sleepTime = 700;
-        this.maxSleepTime = 4000;
-        this.concurrencyLimit = 1;
-        this.attachmentMode = "TM";
+  public constructor() {
+    super();
+    this.maxRunLimit = 1;
+    this.sleepTime = 700;
+    this.maxSleepTime = 4000;
+    this.concurrencyLimit = 1;
+    this.attachmentMode = "TM";
+  }
+
+  public async bookParse(): Promise<Book> {
+    const bookUrl = document.location.href;
+    const bookIDMatch = bookUrl.match(/book\/(\d+)/);
+    const bookID = bookIDMatch ? bookIDMatch[1] : "000";
+
+    const titleInfoUrl = `https://api.kadokado.com.tw/v2/titles/${bookID}`;
+    const titleInfoText = await ggetText(titleInfoUrl);
+    if (!titleInfoText) {
+      throw new Error("抓取作品資訊失敗！");
+    }
+    const titleInfo: KadokadoTitleInfo = JSON.parse(titleInfoText as string);
+
+    const bookname = titleInfo.displayName;
+    const author = titleInfo.ownerDisplayName;
+    const introduction = titleInfo.logline;
+    const introductionHTML = document.createElement("div");
+    introductionHTML.innerText = introduction;
+    const coverUrl = titleInfo.coverUrls?.[0];
+
+    const additionalMetadate: BookAdditionalMetadate = {
+      tags: titleInfo.tags,
+      language: "zh",
+    };
+    if (coverUrl) {
+      getAttachment(coverUrl, this.attachmentMode, "cover-")
+        .then((img) => {
+          additionalMetadate.cover = img;
+        })
+        .catch((error) => log.error(error));
     }
 
-    public async bookParse(): Promise<Book> {
-        const bookUrl = document.location.href;
-        const bookIDMatch = bookUrl.match(/book\/(\d+)/);
-        const bookID = bookIDMatch ? bookIDMatch[1] : '000';
-        const bookname = (document.querySelector("main > section div h1") as HTMLElement)?.innerText;
-        const authorDom = (document.querySelector("main > section > div > div > span > a") as HTMLAnchorElement);
-        const author = authorDom ? authorDom.innerText : "佚名";
-        const authorIDMatch = authorDom?.href.match(/user\/(\d+)/);
-        const authorID = authorIDMatch ? authorIDMatch[1] : '000';
-        const introduction = (document.querySelector("section#introduction p") as HTMLElement)?.innerText;
-        const introductionHTML = document.createElement("div");
-        introductionHTML.innerText = introduction;
-        const coverUrl = (document.querySelector("main > section img") as HTMLImageElement)?.src;
-        const additionalMetadate: BookAdditionalMetadate = {
-            tags: Array.from(document.querySelectorAll("main > section > div > div > div > a")).map((e) => (e as HTMLElement).innerText),
-            language: "zh",
-        };
-        if (coverUrl) {
-            getAttachment(coverUrl, this.attachmentMode, "cover-")
-                .then((img) => {
-                    additionalMetadate.cover = img;
-                })
-                .catch((error) => log.error(error));
-        }
-        const sectionList = Array.from(document.querySelectorAll("section#chapter > div >div"));
-        let chapterNumber = 0;
-        let sectionNumber = 0;
-        let sectionChapterNumber = 0;
-        const chapters: Chapter[] = [];
-        sectionList.forEach((e) => {
-            sectionChapterNumber = 0;
-            sectionNumber++;
-            const sectionName = (e.querySelector("h3") as HTMLElement)?.innerText;
-            const chapterList = Array.from(e.querySelectorAll("ul li"));
-            chapterList.forEach((c) => {
-                const chapterUrl = (c.querySelector("a") as HTMLAnchorElement).href + `?titleId=${bookID}&ownerId=${authorID}`;
-                const ChapterName = (c.querySelector("h4") as HTMLElement).innerText;
-                chapterNumber++;
-                sectionChapterNumber++;
-                chapters.push(new Chapter({
-                    bookUrl,
-                    bookname,
-                    chapterUrl,
-                    chapterNumber: chapterNumber,
-                    chapterName: ChapterName,
-                    isVIP: false,
-                    isPaid: false,
-                    sectionName,
-                    sectionNumber,
-                    sectionChapterNumber,
-                    chapterParse: this.chapterParse,
-                    charset: this.charset,
-                    options: {},
-                }));
-            });
-        });
-        return new Book({
+    const collectionUrl = `https://api.kadokado.com.tw/v3/title/${bookID}/collection`;
+    const collectionText = await ggetText(collectionUrl);
+    if (!collectionText) {
+      throw new Error("抓取章節目錄失敗！");
+    }
+    const collections: KadokadoCollection[] = JSON.parse(
+      collectionText as string
+    );
+
+    let chapterNumber = 0;
+    let sectionNumber = 0;
+    let sectionChapterNumber = 0;
+    const chapters: Chapter[] = [];
+    for (const collection of collections) {
+      sectionChapterNumber = 0;
+      sectionNumber++;
+      const sectionName = collection.collectionDisplayName;
+      for (const ch of collection.chapters) {
+        const chapterUrl = `https://www.kadokado.com.tw/chapter/${ch.chapterId}`;
+        chapterNumber++;
+        sectionChapterNumber++;
+        chapters.push(
+          new Chapter({
             bookUrl,
             bookname,
-            author,
-            introduction,
-            introductionHTML,
-            additionalMetadate,
-            chapters,
-        });
+            chapterUrl,
+            chapterNumber,
+            chapterName: ch.chapterDisplayName,
+            isVIP: !ch.isFree,
+            isPaid: false,
+            sectionName,
+            sectionNumber,
+            sectionChapterNumber,
+            chapterParse: this.chapterParse,
+            charset: this.charset,
+            options: { chapterId: ch.chapterId },
+          })
+        );
+      }
     }
 
-    public async chapterParse(
-        chapterUrl: string,
-        chapterName: string | null,
-        isVIP: boolean,
-        isPaid: boolean | null,
-        charset: string,
-        options: object
-    ): Promise<ChapterParseObject> {
-        const contentRaw = document.createElement("div");
-        contentRaw.innerHTML = (await getFrameContentEvent(chapterUrl))?.querySelector("ul > div")?.innerHTML ?? "";
-        const { dom, text, images } = await cleanDOM(contentRaw, "TM");
-        return {
-            chapterName,
-            contentRaw,
-            contentText: text,
-            contentHTML: dom,
-            contentImages: images,
-            additionalMetadate:null,
-        };
+    return new Book({
+      bookUrl,
+      bookname,
+      author,
+      introduction,
+      introductionHTML,
+      additionalMetadate,
+      chapters,
+    });
+  }
+
+  public async chapterParse(
+    chapterUrl: string,
+    chapterName: string | null,
+    isVIP: boolean,
+    isPaid: boolean | null,
+    charset: string,
+    options: { chapterId: number }
+  ): Promise<ChapterParseObject> {
+    const { chapterId } = options;
+    const contentUrl = `https://api.kadokado.com.tw/v3/chapter/${chapterId}/content`;
+    const contentText = await ggetText(contentUrl);
+    if (!contentText) {
+      throw new Error(`抓取章節內容失敗！chapterId=${chapterId}`);
     }
+    const contentData: KadokadoChapterContent = JSON.parse(
+      contentText as string
+    );
+
+    const contentRaw = document.createElement("div");
+    contentRaw.innerHTML = contentData.content ?? "";
+
+    const { dom, text, images } = await cleanDOM(contentRaw, "TM");
+    return {
+      chapterName,
+      contentRaw,
+      contentText: text,
+      contentHTML: dom,
+      contentImages: images,
+      additionalMetadate: null,
+    };
+  }
 }
